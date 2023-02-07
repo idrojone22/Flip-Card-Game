56cd79fffd9ef78aac0772e57ada78ff68822b99
Jordi <jordivallsplasmx@gmail.com>
2023-02-07 13:22:14 +0100

    Version 2.0

--- main.js
+++ main.js
@@ -1,16 +1,14 @@
 //variables
 let pantalla = document.getElementById('pantalla');
 let randomizarCartes = Math.round (Math.random() * 2);
-let numeroCarta = Math.round (Math.random() / Math.random());
+let numero = 1;
 let numeroPareja = 1;
-let numeroIdentidad = 0;
-let numeroCarta1 = Math.round (Math.random() / Math.random());
-let numeroIdentidad1= 10;
 let contador = 0;
 let primeraCarta;
 let segonaCarta;
 let tiempo = 0;
 let win = 0;
+let order = [];
 
 function jugar() {
   añadirCartas();
@@ -25,169 +23,30 @@ function siguiente_nivel() {
 };
 
 function añadirCartas() {
-  pantalla.innerHTML = '';
+  container.innerHTML = '';
   tiempo = 0;
-
-  if (randomizarCartes == 0) {
-      for (let i = 0; i < numeroPareja; i++) {
-          for (let i = 0; i <1; i++) {
-              pantalla.innerHTML +=  
-              `
-                  <section id = "container">
-                      <div class="flip-card">
-                          <div class="flip-card-inner ${numeroIdentidad}">
-                              <div class="flip-card-front"></div>
-                              <div class="flip-card-back">
-                                  <img class="img" src="https://avatars.dicebear.com/v2/avataaars/${numeroCarta}.jpg">
-                              </div>
-                              </div>
-                          </div>
-                      </div>
-                      <div class="flip-card">
-                          <div class="flip-card-inner ${numeroIdentidad}">
-                              <div class="flip-card-front"></div>
-                                  <div class="flip-card-back">
-                                      <img class="img" src="https://avatars.dicebear.com/v2/avataaars/${numeroCarta}.jpg">
-                                  </div>
-                              </div>
-                          </div>
-                      </div>
-                      <div class="flip-card">
-                          <div class="flip-card-inner ${numeroIdentidad1}">
-                              <div class="flip-card-front"></div>
-                              <div class="flip-card-back">
-                                  <img class="img" src="https://avatars.dicebear.com/v2/avataaars/${numeroCarta1}.jpg">
-                              </div>
-                              </div>
-                          </div>
-                      </div>
-                      <div class="flip-card">
-                          <div class="flip-card-inner ${numeroIdentidad1}">
-                              <div class="flip-card-front"></div>
-                                  <div class="flip-card-back">
-                                      <img class="img" src="https://avatars.dicebear.com/v2/avataaars/${numeroCarta1}.jpg">
-                                  </div>
-                              </div>
-                          </div>
-                      </div>
-                  </section>
-              `
-          };
-          numeroCarta++;
-          numeroIdentidad++;
-          numeroCarta1++;
-          numeroIdentidad1++;
-      };
-      numeroPareja++;
-      numeroPareja++;
-      randomizarCartes = Math.round (Math.random() * 2);
-  } else if ( randomizarCartes == 1) {
-      for (let i = 0; i < numeroPareja; i++) {
-          for (let i = 0; i <1; i++) {
-              pantalla.innerHTML += 
-              `
-                  <section id = "container">
-                      <div class="flip-card">
-                          <div class="flip-card-inner ${numeroIdentidad}">
-                              <div class="flip-card-front"></div>
-                                  <div class="flip-card-back">
-                                      <img class="img" src="https://avatars.dicebear.com/v2/avataaars/${numeroCarta}.jpg">
-                                  </div>
-                              </div>
-                          </div>
-                      </div>
-                      <div class="flip-card">
-                          <div class="flip-card-inner ${numeroIdentidad1}">
-                              <div class="flip-card-front"></div>
-                                  <div class="flip-card-back">
-                                      <img class="img" src="https://avatars.dicebear.com/v2/avataaars/${numeroCarta1}.jpg">
-                                  </div>
-                              </div>
-                          </div>
-                      </div>
-                      <div class="flip-card">
-                          <div class="flip-card-inner ${numeroIdentidad}">
-                              <div class="flip-card-front"></div>
-                                  <div class="flip-card-back">
-                                      <img class="img" src="https://avatars.dicebear.com/v2/avataaars/${numeroCarta}.jpg">
-                                  </div>
-                              </div>
-                          </div>
-                      </div>
-                      <div class="flip-card">
-                          <div class="flip-card-inner ${numeroIdentidad1}">
-                              <div class="flip-card-front"></div>
-                                  <div class="flip-card-back">
-                                      <img class="img" src="https://avatars.dicebear.com/v2/avataaars/${numeroCarta1}.jpg">
-                                  </div>
-                              </div>
-                          </div>
-                      </div>
-              </section>
-            `
-          };
-          numeroCarta++;
-          numeroIdentidad++;
-          numeroCarta1++;
-          numeroIdentidad1++;
-      };
-      numeroPareja++;
-      numeroPareja++;
-      randomizarCartes = Math.round (Math.random() * 2);
-  } else {
-    for (let i = 0; i < numeroPareja; i++) {
-      for (let i = 0; i <1; i++) {
-          pantalla.innerHTML += 
-          `
-              <section id = "container">
-                  <div class="flip-card">
-                      <div class="flip-card-inner ${numeroIdentidad1}">
-                          <div class="flip-card-front"></div>
-                              <div class="flip-card-back">
-                                  <img class="img" src="https://avatars.dicebear.com/v2/avataaars/${numeroCarta1}.jpg">
-                              </div>
-                          </div>
-                      </div>
-                  </div>
-                  <div class="flip-card">
-                      <div class="flip-card-inner ${numeroIdentidad}">
-                          <div class="flip-card-front"></div>
-                              <div class="flip-card-back">
-                                  <img class="img" src="https://avatars.dicebear.com/v2/avataaars/${numeroCarta}.jpg">
-                              </div>
-                          </div>
-                      </div>
-                  </div>
-                  <div class="flip-card">
-                      <div class="flip-card-inner ${numeroIdentidad}">
-                          <div class="flip-card-front"></div>
-                              <div class="flip-card-back">
-                                  <img class="img" src="https://avatars.dicebear.com/v2/avataaars/${numeroCarta}.jpg">
-                              </div>
-                          </div>
-                      </div>
-                  </div>
-                  <div class="flip-card">
-                      <div class="flip-card-inner ${numeroIdentidad1}">
-                          <div class="flip-card-front"></div>
-                              <div class="flip-card-back">
-                                  <img class="img" src="https://avatars.dicebear.com/v2/avataaars/${numeroCarta1}.jpg">
-                              </div>
-                          </div>
-                      </div>
-                  </div>
-          </section>
-          `
-      };
-      numeroCarta++;
-      numeroIdentidad++;
-      numeroCarta1++;
-      numeroIdentidad1++;
+  for (let i = 0; i < numeroPareja; i++) {
+    for (let i = 0; i < 2; i++) {
+      order.push(numero)
+      order.sort( () => Math.random() - 0.5)
+    };
+  numero++;    
   };
-  numeroPareja++;
-  numeroPareja++;
-  randomizarCartes = Math.round (Math.random() * 2);
-  }
+  order.forEach( numero => {
+    container.innerHTML += 
+    `
+    <div class="flip-card">
+      <div class="flip-card-inner ${numero}">
+        <div class="flip-card-front"></div>
+          <div class="flip-card-back">
+            <img class="img" src="https://avatars.dicebear.com/v2/avataaars/${numero}.jpg">
+          </div>
+        </div>
+      </div>
+    </div>
+    ` 
+  });
+  order = [];
 };
 
 function rotarCarta() {
